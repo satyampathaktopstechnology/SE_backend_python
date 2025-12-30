9fdeff13e1366f9cb7cf74ab5f3733bd1997d3d2
satyampathaktopstechnology <satyampathak.topstechnology@gmail.com>
2025-12-30 11:53:53 +0530

    first commit

---
+++ Practice/factorial.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number to find factorial "))
+fact=1
+while(a!=0):
+    fact*=a
+    a-=1
+
+    print(fact)
+
+    
+    
---
+++ Practice/fibonacci.py
@@ -0,0 +1,7 @@
+a = 0
+b = 1
+
+for i in range(1, 6):
+    print(a)
+    a, b = b, a + b
+    
