f61541098d9cb3439332f42180f04dcbdc7f5f48
satyampathaktopstechnology <satyampathak.topstechnology@gmail.com>
2026-01-10 10:56:31 +0530

    firdt commit

---
+++ Practice/diamond_pattern.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    for j in range(5-i):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print("*  ",end=" ")
+    print()
+for l in range(4):
+    for m in range((l+1)+1):
+        print(" ",end=" ")
+    for n in range(4-l):
+        print("*  ",end=" ")
+    print()
