889b7191991be586da8f5b887de9552dfc721b38
satyampathaktopstechnology <satyampathak.topstechnology@gmail.com>
2026-02-04 12:14:42 +0530

    first commit

---
+++ Practice/string.py
@@ -0,0 +1,14 @@
+str="Rahul is energetic"
+print(str.casefold())
+print(str.capitalize())
+print(str.center(25,"*"))
+print(str.count(str))
+print(str.expandtabs(52))
+print(str.find("x"))
+print(str.replace("R","r"))
+print(str.isalnum())
+print(str.isalpha())
+print(str.rindex("energetic"))
+
+
+
